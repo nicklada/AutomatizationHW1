f8a498f63713191bad6e55fbadc22360838e6ab1
lada <nicklada94@gmail.com>
2020-05-25 15:54:30 +0300

    Tests added

--- src/main/java/ru/netology/unit/CashbackHackService.java
+++ src/main/java/ru/netology/unit/CashbackHackService.java
@@ -4,6 +4,9 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
+        if (amount == boundary) {
+            return boundary - amount;
+        }
         return boundary - amount % boundary;
     }
 }
